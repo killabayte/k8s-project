3c74753941323f24fed4a3b73f841de4b5bf8f19
ykostiannikov <Yaroslav.Kostiannikov@lookout.com>
2018-06-17 14:45:08 +0300

    add all other thing to repo

--- Advanced_K8S_usage/Auditing/minikubeStartWithAudit.sh
+++ Advanced_K8S_usage/Auditing/minikubeStartWithAudit.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-minikube start  --extra-config=apiserver.Authorization.Mode=RBAC --extra-config=apiserver.Audit.LogOptions.Path=/var/logs/audit.log   --extra-config=apiserver.Audit.PolicyFile=/etc/kubernetes/addons/audit-policy.yaml
+#!/bin/bash
+#Debugging command
+minikube -v=10 --kubernetes-version v1.8.0 --cpus=4 --memory=8048 --extra-config=apiserver.Authorization.Mode=RBAC --extra-config=apiserver.Audit.LogOptions.Path=/var/log/audit.log --extra-config=apiserver.Audit.PolicyFile=/etc/kubernetes/addons/audit-policy.yaml
+#Worked for me, command
+#But this way, requires intrusion to kube api server manifest on minikube node and add some volume for path with audit-policy and logs
+minikube start --extra-config=apiserver.authorization-mode=RBAC --extra-config=apiserver.audit-log-path=/var/log/audit.log --extra-config=apiserver.audit-policy-file=/etc/ssl/certs/audit-policy.yaml
